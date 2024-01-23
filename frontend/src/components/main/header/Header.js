8a75e9a5670446060ca4b948b01e855c1d9c63b3
dabbler2 <darkmagu94@gmail.com>
2024-01-23 09:44:30 +0900

    Merge pull request #7 from kim-sunah/host2
    Host2

--- frontend/src/components/main/header/Header.js
+++ frontend/src/components/main/header/Header.js
@@ -7,28 +7,28 @@ import "./style.css";
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-    const [acessToken , setacessToken] = useState()
-    useEffect(()=>{
+    const [acessToken, setacessToken] = useState()
+    useEffect(() => {
         setacessToken(sessionStorage.getItem("accessToken"))
-        
-    },[])
+
+    }, [])
 
     return (
         <header>
             <div className="div-header">
                 <div className="navbar">
-                    {!acessToken && <Link to = "Login" className="list-item-link-LOGIN">LOGIN</Link>}
-                    {acessToken   && <Link to ="" className="list-item-link-LOGIN">LOGOUT</Link>}
+                    {!acessToken && <Link to="Login" className="list-item-link-LOGIN">LOGIN</Link>}
+                    {acessToken && <Link to="" className="list-item-link-LOGIN">LOGOUT</Link>}
                     <div className="overlap">
                         <div className="list-item-link-JOIN">JOIN US</div>
-                    
+
                     </div>
                     <div className="list-item-link-CART">CART</div>
                     <div className="list-item-link">
                         <div className="div">0</div>
                     </div>
                     <div className="list-item-link-ORDER">ORDER</div>
-                    <Link to ="mypage" className="list-item-link-MY">MY PAGE</Link>
+                    <Link to="mypage" className="list-item-link-MY">MY PAGE</Link>
                     <div className="list-item-2" />
                     <div className="list-item-link-2">BOOKMARK</div>
                     <div className="list-item-3" />
@@ -38,12 +38,35 @@ const Header = () => {
                     <div className="list-item-link-4">상품문의</div>
                     <div className="list-item-link-5">교환/반품/취소</div>
                     <div className="list-item-link-6">REVIEW</div>
-                 
-                </div>
-             
-              
 
-                
+                </div>
+                <div className="heading-link" />
+                <div className="form-fieldset">
+                    <input></input>
+                </div>
+                <div className="navbar-2">
+                    <div className="div-df-allmenu-btn">
+                        <div className="span-df-allmenu-btn" />
+                        <div className="span-df-allmenu-btn-2" />
+                        <div className="span-df-allmenu-btn-3" />
+                    </div>
+                    <div className="list-item-link-BEST">BEST 10%</div>
+                    <div className="list-item-link-NEW">NEW 10%</div>
+                    <div className="list-item-link-OUTER">OUTER</div>
+                    <div className="list-item-link-TOP">TOP</div>
+                    <div className="list-item-link-KNIT">KNIT</div>
+                    <div className="list-item-link-PANTS">PANTS</div>
+                    <div className="list-item-link-SKIRT">SKIRT</div>
+                    <div className="list-item-link-DRESS">DRESS</div>
+                    <div className="list-item-link-ACC">ACC</div>
+                    <div className="list-item-link-SHOES">SHOES&amp;BAG</div>
+                    <div className="list-item-link-7">SEASON OFF</div>
+                    <div className="list-item-link-ONLY">ONLY YOU</div>
+                </div>
+
+
+
+
             </div>
         </header>
     );
