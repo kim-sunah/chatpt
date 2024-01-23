c7a04d51e7637fd84888c7e8cb9fe1d912ced565
CHOI TAE YOUNG <chlxodud04@nave.rcom>
2024-01-23 21:30:53 +0900

    admin page

--- frontend/src/components/admin/Main.js
+++ frontend/src/components/admin/Main.js
@@ -1,80 +1,66 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Main.css";
 
 export const Main = () => {
-    useEffect(() =>{
+    const [Client, setClient] = useState()
+    const [User, setUser] = useState("")
+    useEffect(() => {
+        fetch("http://localhost:4000/users/Alluser", { method: "GET", headers: { "Content-Type": "application/json", "Authorization": "Bearer " + sessionStorage.getItem("accessToken"), "refreshtoken": sessionStorage.getItem("refreshToken") } }).then(res => res.json()).then(resData => { setClient(resData.user.userCount); setUser(resData.user.user) }).catch(err => console.log(err))
 
-    },[])
-  return (
-    <div className="main">
-      <div className="div-container">
-        <div className="div-grid">
-          <div className="div-flex-2">
-            <div className="div-2">
-              <div className="text-wrapper-3">Total clients</div>
-              <div className="text-wrapper-4">6389</div>
-            </div>
-          </div>
-          <div className="div-flex-3">
-           
-            <div className="div-3">
-              <div className="text-wrapper-5">Total product</div>
-              <div className="text-wrapper-6">$ 46,760.89</div>
-            </div>
-          </div>
-          <div className="div-flex-4">
-          
-            <div className="div-4">
-              <div className="text-wrapper-7">New sales</div>
-              <div className="text-wrapper-8">376</div>
-            </div>
-          </div>
-         
-        </div>
-        <div className="div-w-full">
-          <div className="table">
-            <div className="header-row">
-              <div className="cell-client">CLIENT</div>
-              <div className="cell-amount">AMOUNT</div>
-              <div className="cell-status">STATUS</div>
-              <div className="cell-date">DATE</div>
-            </div>
-            <div className="body">
-                <div>
-                    heelo
+    }, [])
+    return (
+        <div className="main">
+            <div className="div-container">
+                <div className="div-grid">
+                    <div className="div-flex-2">
+                        <div className="div-2">
+                            <div className="text-wrapper-3">Total clients</div>
+                            <div className="text-wrapper-4">{Client}</div>
+                        </div>
+                    </div>
+                    <div className="div-flex-3">
+
+                        <div className="div-3">
+                            <div className="text-wrapper-5">Total product</div>
+                            <div className="text-wrapper-6">0</div>
+                        </div>
+                    </div>
+                    <div className="div-flex-4">
+                        <div className="div-4">
+                            <div className="text-wrapper-7">New sales</div>
+                            <div className="text-wrapper-8">0</div>
+                        </div>
+                    </div>
                 </div>
+
+                <table className="div-w-full">
+                    <tr className="header-row">
+                        <th scope="col">이름</th>
+                        <th scope="col">성별</th>
+                        <th scope="col">유저 접속 경로</th>
+                        <th scope="col">유저 권한</th>
+                        <th scope="col">사용 여부</th>
+                    </tr>
+                    {User && User.map(user => (
+                            <tr key={user.id} className="userlist">
+                                <td className="nicknameli">{user.nickname ? user.nickname : "null"}</td>
+                                <td>{user.gender ? user.gender : "null"}</td>
+                                <td >{user.phone ? user.phone : "null"}</td>
+                                <td >{user.createdAt ? user.createdAt : "null"}</td>
+                            </tr>
+                        ))
+                    }
+
+
+                </table>
+
+
             </div>
-          </div>
-          <div className="div-grid-2">
-            <div className="span-flex">
-              <div className="showing-of">SHOWING 21-30 OF 100</div>
-            </div>
-            <div className="span-flex-2">
-              <div className="nav-table-navigation-2">
-                <div className="text-wrapper-19">1</div>
-              </div>
-              <div className="nav-table-navigation-3">
-                <div className="text-wrapper-20">2</div>
-              </div>
-              <div className="nav-table-navigation-4">
-                <div className="text-wrapper-21">3</div>
-              </div>
-              <div className="nav-table-navigation-5">
-                <div className="text-wrapper-20">4</div>
-              </div>
-              <div className="nav-table-navigation-6">
-                <div className="text-wrapper-22">...</div>
-              </div>
-              <div className="nav-table-navigation-7">
-                <div className="text-wrapper-20">8</div>
-              </div>
-              <div className="nav-table-navigation-8">
-                <div className="text-wrapper-20">9</div>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
+
+export default Main;
+
+
+
