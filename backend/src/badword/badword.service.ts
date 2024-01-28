304299d44be10899bbb78ee87b1ec710bde9444f
dabbler2 <117023230+dabbler2@users.noreply.github.com>
2024-01-28 13:28:53 +0900

    레디스 연결

--- backend/src/badword/badword.service.ts
+++ backend/src/badword/badword.service.ts
@@ -2,26 +2,48 @@ import { Injectable } from '@nestjs/common'
 import { Repository } from 'typeorm'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Badword } from '../entities/badword.entity'
-import { Cron, CronExpression } from '@nestjs/schedule'	
+import { Cron, CronExpression } from '@nestjs/schedule'
+import Redis from 'ioredis'
 
 @Injectable()
 export class BadwordService {
+	private redisClient: Redis
+	private badwordList: string[]
+
 	constructor(
 		@InjectRepository(Badword) private readonly badwordRepository: Repository<Badword>
-	){}
+	){
+		this.redisClient = new Redis({
+			host: 'localhost',
+			port: 6379
+		})
+	}
 	
-	private pollingInterval: number = 5000
 	
-	//@Cron('*/5 * * * * *')
-	//handleInterval() {
-	//	console.log('Called every 5 seconds');
-	//}
+	@Cron('*/10 * * * * *')
+	async handleInterval() {
+		console.log(new Date())
+		if(await this.redisClient.get('updated')){
+			console.log(await this.redisClient.get('badwords'))
+			await this.redisClient.set('updated','')
+		}
+	}
 	
-	async createBadword(badwords: string[]){
-		return await this.badwordRepository.createQueryBuilder()
-			.insert()
-			.values(badwords.map(badword => ({badword})))
-			.orIgnore()
-			.execute()
+	async cacheBadwords(){
+		const badwords = await this.badwordRepository.find()
+		await this.redisClient.set('badwords',JSON.stringify(badwords.map(badword => badword.badword)))
+		await this.redisClient.set('updated','YES')
+	}
+	
+	async createBadword(newBadwords: string[]){
+		try{
+			const res = await this.badwordRepository.createQueryBuilder()
+				.insert()
+				.values(newBadwords.map(badword => ({badword})))
+				.orIgnore()
+				.execute()
+			if(res.raw.affectedRows) await this.cacheBadwords()
+			return res
+		}catch(e){throw e}
 	}
 }
