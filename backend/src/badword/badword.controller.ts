1a44517fb54e2a5d0cd3cecc4a546e5d46ad40b0
dabbler2 <117023230+dabbler2@users.noreply.github.com>
2024-01-28 16:24:13 +0900

    금칙어 기본 틀 완료

--- backend/src/badword/badword.controller.ts
+++ backend/src/badword/badword.controller.ts
@@ -6,8 +6,15 @@ import {BadwordDto} from './dtos/badword.dto'
 export class BadwordController {
 	constructor(private readonly badwordService: BadwordService) {}
 	
+	// 금칙어 추가
 	@Post('')
 	async createBadword(@Body() body: BadwordDto){
 		return await this.badwordService.createBadword(body.badwords)
+	}
+	
+	// 금칙어 검색
+	@Get('')
+	searchBadword(){
+		return this.badwordService.searchBadword()
 	}
 }
