7a09c7e136a3b47990fd352de00efa0f8274e172
dabbler2 <117023230+dabbler2@users.noreply.github.com>
2024-01-30 09:13:28 +0900

    금칙어 추가

--- backend/src/badword/badword.controller.ts
+++ backend/src/badword/badword.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Body, Get, Post } from '@nestjs/common'
+import { Controller, Body, Get, Post, UseGuards } from '@nestjs/common'
 import {BadwordService} from './badword.service'
 import {BadwordDto} from './dtos/badword.dto'
+import { RoleGuard } from '../auth/guard/role.guard';
+import { Role } from '../enum/Role';
+import { Roles } from 'src/auth/decorators/roles.decorator';
 
 @Controller('badword')
 export class BadwordController {
 	constructor(private readonly badwordService: BadwordService) {}
 	
 	// 금칙어 추가
+	@UseGuards(RoleGuard)
+    @Roles(Role.Admin)
 	@Post('add')
 	async createBadword(@Body() body: BadwordDto){
 		return await this.badwordService.createBadword(body.badwords)
