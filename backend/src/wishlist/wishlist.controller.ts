52bd8b9b54d0e145f518ea404e72e4c07ca7b6c2
dabbler2 <117023230+dabbler2@users.noreply.github.com>
2024-01-30 13:26:12 +0900

    찜 등록 구현

--- backend/src/wishlist/wishlist.controller.ts
+++ backend/src/wishlist/wishlist.controller.ts
@@ -1,13 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Param, UseGuards } from '@nestjs/common';
 import { WishlistService } from './wishlist.service'
 import { RoleGuard } from '../auth/guard/role.guard';
 import { Role } from '../enum/Role';
 import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Id } from 'src/util/id'
 
+@UseGuards(RoleGuard)
 @Controller('wishlist')
 export class WishlistController {
 	constructor(private readonly wishlistService: WishlistService) {}
 	
 	// 찜 등록
-	
+	@Roles(Role.User)
+	@Post(':id')
+	async createWish(@Param() param: Id){
+		return await this.wishlistService.createWish(param.id)
+	}
 }
--- backend/src/wishlist/wishlist.service.ts
+++ backend/src/wishlist/wishlist.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException, Scope, Inject } from '@nestjs/common'
+import { Repository, QueryFailedError } from 'typeorm'
+import { InjectRepository } from '@nestjs/typeorm'
+import {Wishlist} from '../entities/wishlist.entity'
+import { REQUEST } from '@nestjs/core'
+import { Request } from 'express'
 
-@Injectable()
-export class WishlistService {}
+@Injectable({scope: Scope.REQUEST})
+export class WishlistService {
+	constructor(
+		@InjectRepository(Wishlist) private readonly wishlistRepository: Repository<Wishlist>,
+		@Inject(REQUEST) private readonly req: Request
+	){}
+	
+	async createWish(product_id: number){
+		try{
+			return await this.wishlistRepository.save({user_id:this.req.user['id'],product_id})
+		}catch(e){
+			if(e instanceof QueryFailedError) throw new BadRequestException('해당 강의가 존재하지 않거나 이미 찜한 강의입니다.')
+		}
+	}
+}
