8270233a2dbb4e197a7ace928e3e40f3374a40bc
SEWOONG KIM <gozneokhan@gmail.com>
2024-02-02 11:38:32 +0900

    .

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,8 +2,8 @@
 
 PROJECT_ROOT="/home/ubuntu/app"
 REACT_ROOT="/home/ubuntu/app/frontend"
-APP_NAME="project"
-REACT_APP_NAME="project"
+APP_NAME="npm start"
+REACT_APP_NAME="npm start"
 PM2_PATH="/home/ubuntu/.nvm/versions/node/v21.5.0/bin/pm2"
 
 TIME_NOW=$(date +%c)
@@ -14,7 +14,7 @@ pm2 delete $APP_NAME
 cd backend
 pm2 start npm --name $APP_NAME -- start
 
-cd $REACT_ROOT
-pm2 start npm --name $REACT_APP_NAME -- start
+cd $REACT_ROOT 
+pm2 start npm --name $REACT_APP_NAME-- start
 
 echo "$TIME_NOW > Deploy has been completed"
